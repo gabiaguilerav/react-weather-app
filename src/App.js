2decc4a0cb3982bbf1aa9cd7e680ae7500fd745a
Gabriela Aguilera <gabiaguilerava@gmail.com>
2024-06-24 13:29:42 -0600

    Footer HTML and CSS re-added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Footer";
 import Temperature from "./Temperature";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <div className="container">
         <Temperature />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/gabiaguilerav" target={"_blank"}>
+          Gabriela Aguilera
+        </a>{" "}
+        and is on{" "}
+        <a href="https://github.com/gabiaguilerav/meteo.git" target={"_blank"}>
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="#" target={"_blank"}>
+          Netlify{" "}
+        </a>
+      </p>
+    </div>
+  );
+}
