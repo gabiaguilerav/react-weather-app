57431d3ac7afa61e9ee64a4f45565b0fa5edfc8e
Gabriela Aguilera <gabiaguilerava@gmail.com>
2024-06-24 13:37:25 -0600

    Footer bug fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import "./App.css";
-import Footer from "./Footer";
 import Temperature from "./Temperature";
+import "./Footer.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Temperature />
-        <Footer />
+        <div className="footer">
+          <p>
+            This project was coded by{" "}
+            <a href="https://github.com/gabiaguilerav" target={"_blank"}>
+              Gabriela Aguilera
+            </a>{" "}
+            and is on{" "}
+            <a
+              href="https://github.com/gabiaguilerav/meteo.git"
+              target={"_blank"}
+            >
+              Github
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://gabriela-react-weather-app.netlify.app/"
+              target={"_blank"}
+            >
+              Netlify{" "}
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
