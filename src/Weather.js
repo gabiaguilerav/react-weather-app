a4b1551284c8ab62ea6d3d6260b222d99d250682
Gabriela Aguilera <gabiaguilerava@gmail.com>
2024-06-25 12:26:45 -0600

    API improved

--- src/Weather.js
+++ src/Weather.js
@@ -97,7 +97,7 @@ export default function Weather() {
   } else {
     let city = "Maastricht";
     let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${"Maastricht"}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
 
     return <div>{form}</div>;
